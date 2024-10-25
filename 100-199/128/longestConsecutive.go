4bbd14b658e0c065f0e0464d423b8a57f90f4ddb
Aneesh Rayala <aneeshr0302@gmail.com>
2024-10-25 17:49:47 -0500

    feat: 128 Longest Consecutive Sequence

---
+++ 100-199/128/longestConsecutive.go
@@ -0,0 +1,21 @@
+package main 
+
+func longestConsecutive(nums []int) int {
+	set := make(map[int]bool)
+	longest := 0
+
+	for _, num := range nums {
+		set[num] = true
+	}
+
+	for _, num := range nums {
+		if !set[num - 1] {
+			length := 0
+			for set[num + length] {
+				length++
+			}
+			longest = max(longest, length)
+		}
+	}
+	return longest
+}
