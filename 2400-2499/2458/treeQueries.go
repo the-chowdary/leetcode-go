1c211908e22edc62faabb4f4913e60b3735e6a2c
Aneesh Rayala <aneeshr0302@gmail.com>
2024-10-25 22:38:05 -0500

    feat: 2458 Height of Binary Tree After Subtree Removal Queries

---
+++ 2400-2499/2458/treeQueries.go
@@ -0,0 +1,44 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func treeQueries(root *TreeNode, queries []int) []int {
+    height := map[*TreeNode]int{}
+
+    var treeHeight func(root *TreeNode) int
+
+    treeHeight = func(root *TreeNode) int {
+        if root == nil {
+            return 0
+        }
+        height[root] = 1 + max(treeHeight(root.Left), treeHeight(root.Right))
+        return height[root]
+    }
+    treeHeight(root)
+
+    result := make([]int, len(height) + 1)
+
+    var dfs func(root *TreeNode, depth int, restHeight int) 
+
+    dfs = func(root *TreeNode, depth int, restHeight int) {
+        if root == nil {
+            return
+        }
+        depth++
+        result[root.Val] = restHeight
+        dfs(root.Left, depth, max(restHeight, depth + height[root.Right]))
+        dfs(root.Right, depth, max(restHeight, depth + height[root.Left]))
+    }
+    dfs(root, -1, 0)
+
+    for i, q := range queries {
+        queries[i] = result[q] 
+    }
+    return queries
+}
